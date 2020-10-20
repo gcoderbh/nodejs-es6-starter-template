a108fb7fcbab9d7b3b04dfcff7176147a619ad8b
Prajak Jaikam <gcoderbh@gmail.com>
2020-10-20 13:48:26 +0700

    Update test coverage

--- __test__/index.spec.js
+++ __test__/index.spec.js
@@ -1,6 +1,12 @@
 import request from 'supertest';
 import app from '../src/app';
 
-it('return 200 on get base url', async () => {
-  return request(app).get('/').expect(200);
+describe('Server starting test', () => {
+  it('return 200 on get base url', async () => {
+    return request(app).get('/').expect(200);
+  });
+
+  it('return 404 on post base url', async () => {
+    return request(app).post('/').expect(404);
+  });
 });
